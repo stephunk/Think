d54f16004601a1d64ea5ecac1336081598a59ced
Stefan Christian Cismarescu <stefc95@gmail.com>
2019-03-20 21:25:29 -0400

    Create react app and restructured file tree
    Created react app.
    Removed standard code
    Restructured file tree to improve organisation

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import './css/App.css';
+
+/**
+ * Main App class to be rendered in the page
+ */
+class App extends Component {
+  /**
+   * Returns html compnenets to be rendered.
+   * @return {html} html component
+   */
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        asd
+        </header>
+
+      </div>
+    );
+  }
+}
+
+export default App;
