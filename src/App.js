47742ffe793a457cdf64a34a6dabc238b246a0b7
Stefan Christian Cismarescu <stefc95@gmail.com>
2019-03-24 11:13:09 -0400

    Adde router + 3 routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import React, {Component} from 'react';
 import './css/App.css';
+import {getUsHousePriceIndex} from './apiWrapper.js';
+import {Router, Route, Switch} from 'react-router-dom';
+import Employment from './components/employment';
+const createBrowserHistory = require('history').createBrowserHistory;
+
+const history = createBrowserHistory();
 
 /**
  * Main App class to be rendered in the page
  */
 class App extends Component {
   /**
+ * Main app constructor sets state.
+ */
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  /**
+   * Override for standard React lifecycle method.
+   * Used for calling data APIs.
+  */
+  componentDidMount() {
+    getUsHousePriceIndex()
+        .then((data) => this.setState({usHousePriceIndex: data}));
+  }
+
+  /**
    * Returns html compnenets to be rendered.
    * @return {html} html component
    */
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        asd
-        </header>
-
-      </div>
+      <Router history={history} basename='/'>
+        <div className='App-header'>
+          <Switch>
+            <Route exact path='/'
+              component={() =>
+                <Employment indexes={this.state.usHousePriceIndex} />}
+            />
+            {/* <Route exact path='/' component={React.component}/>
+            <Route exact path='/login' component={React.component} /> */}
+            <Route render={function() {
+              return <p> Not Found </p>;
+            }} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
